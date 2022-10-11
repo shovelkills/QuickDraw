bbdc67578a1a72a57503fc504c6856107dfe6d38
klow358 <klow358@aucklanduni.ac.nz>
2022-10-11 19:55:26 +1300

    Feature: Added the pie chart of individual difficulty
    Added a pie chart that shows the difficulties of the accuracy wins and
    losses

--- src/main/java/nz/ac/auckland/se206/Graph.java
+++ src/main/java/nz/ac/auckland/se206/Graph.java
@@ -1,9 +1,16 @@
 package nz.ac.auckland.se206;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
 
@@ -34,17 +41,78 @@ public class Graph {
    * @return the generated graph from the data
    */
   public static XYChart.Series<String, Number> getLineChartData() {
+    // Initializes the Linecart X and Y values
     CategoryAxis xAxis = new CategoryAxis();
     NumberAxis yAxis = new NumberAxis();
     LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
     lineChart.setTitle("Time taken per Game");
     XYChart.Series<String, Number> series = new XYChart.Series<>();
+    // Gets the data from the list
     for (int i = 0; i < Users.getWordHistory().size(); i++) {
-      System.out.println(i);
       series.getData().add(
           new Data<String, Number>(Users.getWordHistory().get(i), Users.getTimeHistory().get(i)));
     }
 
     return series;
+  }
+
+  public static ObservableList<PieChart.Data> getWinsLossPieChart() {
+    int totalGames = Users.getWins() + Users.getLosses();
+    double winPercentage = ((double) Users.getWins()) * 100 / totalGames;
+    double lossPercentage = ((double) Users.getLosses()) * 100 / totalGames;
+    ObservableList<PieChart.Data> winLossPieChart = FXCollections.observableArrayList(
+        new PieChart.Data("Wins", winPercentage), new PieChart.Data("Losses", lossPercentage));
+    return winLossPieChart;
+  }
+
+  public static ObservableList<PieChart.Data> getIndividualDifficultyPieChart(String gameOutcome,
+      String Difficulty) {
+
+    boolean wantedGameOutcome = true;
+    int totalGames = Users.getWins();
+    List<String> checkOutcomeDifficulty;
+    // Finds which difficulty they want to look at
+    if (Difficulty.equals("accuracy")) {
+      checkOutcomeDifficulty = Users.getAccuracyDifficultyHistory();
+    } else if (Difficulty.equals("word")) {
+      checkOutcomeDifficulty = Users.getWordDifficultyHistory();
+    } else if (Difficulty.equals("time")) {
+      checkOutcomeDifficulty = Users.getTimeDifficultyHistory();
+    } else {
+      checkOutcomeDifficulty = Users.getConfidenceDifficultyHistory();
+    }
+
+    // Checks if it wants to counts wins or losses
+    if (gameOutcome.equals("Loss")) {
+      wantedGameOutcome = false;
+      totalGames = Users.getLosses();
+    }
+    // Gets all the values for the respective game outcome
+    List<String> gameOutcomeDifficulty = new ArrayList<String>();
+    for (int i = 0; i < Users.getWinHistory().size(); i++) {
+      if (Users.getWinHistory().get(i) == wantedGameOutcome) {
+        gameOutcomeDifficulty.add(checkOutcomeDifficulty.get(i));
+      }
+    }
+
+    // Counts the percentage of each difficulty in the game outcome
+    HashMap<String, Double> countDifficulty = new HashMap<String, Double>();
+    countDifficulty.put("Easy",
+        (double) (Collections.frequency(gameOutcomeDifficulty, "E") * 100 / totalGames));
+    countDifficulty.put("Medium",
+        (double) (Collections.frequency(gameOutcomeDifficulty, "M") * 100 / totalGames));
+    countDifficulty.put("Hard",
+        (double) (Collections.frequency(gameOutcomeDifficulty, "H") * 100 / totalGames));
+    countDifficulty.put("Master",
+        (double) (Collections.frequency(gameOutcomeDifficulty, "MS") * 100 / totalGames));
+
+    ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+    // Adds data to the pieChart Data
+    for (Entry<String, Double> entry : countDifficulty.entrySet()) {
+      pieChartData.add(new PieChart.Data(entry.getKey(), entry.getValue()));
+    }
+
+    return pieChartData;
+
   }
 }
--- src/main/java/nz/ac/auckland/se206/GraphController.java
+++ src/main/java/nz/ac/auckland/se206/GraphController.java
@@ -2,10 +2,29 @@ package nz.ac.auckland.se206;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 
 public class GraphController {
+
+  @FXML
+  private Button lossAccuracyButton;
+
+  @FXML
+  private Button winAccuracyButton;
+
   @FXML
   private LineChart<String, Number> timeLineChart;
+
+  @FXML
+  private PieChart winsLossPieChart;
+
+  @FXML
+  private PieChart individualDifficultyPieChart;
+
+  private static String difficulty;
+
+  private static String gameOutcome;
 
   public void initialize() {
     loadGraphData();
@@ -13,5 +32,28 @@ public class GraphController {
 
   public void loadGraphData() {
     timeLineChart.getData().add(Graph.getLineChartData());
+    winsLossPieChart.setData(Graph.getWinsLossPieChart());
   }
+
+  public void onShowWinsAccuracy() {
+    gameOutcome = "Win";
+    difficulty = "accuracy";
+    loadIndividualDifficulyPieChart();
+
+
+  }
+
+  public void onShowLossAccuracy() {
+    gameOutcome = "Loss";
+    difficulty = "accuracy";
+    loadIndividualDifficulyPieChart();
+
+  }
+
+  public void loadIndividualDifficulyPieChart() {
+    individualDifficultyPieChart
+        .setData(Graph.getIndividualDifficultyPieChart(gameOutcome, difficulty));
+
+  }
+
 }
